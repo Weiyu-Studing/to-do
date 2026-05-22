775eb5b8b8057a053bd78102930334d7f92ab7ce
weiyu <yin1532776643@gmail.com>
2026-05-22 16:28:52 -0500

    bug fix

--- src/components/Managementnotes.js
+++ src/components/Managementnotes.js
@@ -4,7 +4,7 @@ import { formatDate } from './Timeformat';
 function Managementnotes({ noteList, dispatch, setSelectedNote }) {
   return (
     <div className="task-list">
-      {noteList.map(note => (
+      {(noteList || []).map(note => (
         <div className={`task ${note.done ? 'completed' : ''}`} key={note.id}>
           <div className={`status-bar ${note.done ? 'green' : 'blue'}`}></div>
           <div className="task-text">{note.title}</div>
